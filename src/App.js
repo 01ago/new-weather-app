660959f2d67c2c9c2654d070b82d1c5db55bc760
01ago <codingmoding01@gmail.com>
2021-04-04 12:44:39 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from "./Weather"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+         hello world
+        </p>
+        <Weather city="ottawa"/>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from 'axios';
+import Loader from "react-loader-spinner";
+
+export default function Weather(props){
+    function handleResponse(response){
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp}°C`);}
+    let apiKey= "5e495a0f30ae782754537f5c56c584c1";
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return (
+        <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+    );
+}
