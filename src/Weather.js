7baa76c3f8b4260794e721900b4663773fa2b48c
01ago <codingmoding01@gmail.com>
2021-04-04 21:50:20 -0400

    Update Weather.js
    react weather app uploaded

--- src/Weather.js
+++ src/Weather.js
@@ -2,19 +2,120 @@ import React from "react";
 import axios from 'axios';
 import Loader from "react-loader-spinner";
 
-export default function Weather(props){
-    function handleResponse(response){
-        alert(`The weather in ${response.data.name} is ${response.data.main.temp}°C`);}
-    let apiKey= "5e495a0f30ae782754537f5c56c584c1";
-    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    return (
-        <Loader
-        type="Puff"
-        color="orange"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
-    );
+
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Milano",
+    country: "Italy",
+    update: "Last updated 10:00",
+    feelsLike: 3,
+    humidity: 25,
+    wind: 6
+  };
+  return (
+    <div className="Weather">
+      <div className="weather-app backImage">
+        <form className="search-form search-form">
+          <span>
+            <input
+              type="search"
+              placeholder="  Type city"
+              autocomplete="off"
+              autofocus="on"
+              className="city-input"
+            />
+            <input type="submit" value="Search" className="btn" />
+            <button className="btnLocation">Current Location</button>
+          </span>
+        </form>
+        <span className="cityspan">
+          <h1 className="city">{weatherData.city}</h1>{" "}
+        </span>
+        <span className="countryspan">
+          <h1 className="country">{weatherData.country} </h1>
+        </span>
+
+        <div className="date"> {weatherData.update}</div>
+
+        <div className="row">
+          <div className="col-6 weather-temp">
+            <img src="" className="weathericon" alt="" />
+            <p></p>
+            <img src="" alt="" className="mainicon" />
+            <span className="temperature"> 20</span>
+            <span className="units">
+              <a href="/" className="celcius active">
+                °C
+              </a>{" "}
+              |
+              <a href="/" className="fahrenheit">
+                °F
+              </a>
+            </span>
+            <p className="feelslike">
+              Feels like: {weatherData.feelsLike}°{" "}
+              <span className="feels"> </span>
+            </p>
+          </div>
+          <span className="humidp">
+            {" "}
+            Humidity: {weatherData.humidity} <span className="humid"> </span> %{" "}
+          </span>
+          <span className="windp">
+            {" "}
+            Wind: {weatherData.wind}
+            <span className="wind"></span> Km/h{" "}
+          </span>
+          <div className="nextdays">Next 3 days</div>
+          <div className="row weather-forecast">
+            <div className="col-2">
+              <h3> Sun</h3>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png"
+                alt=""
+              />
+              <div className="weather-forecast-temperature">
+                <strong>22°</strong> 17°
+              </div>
+            </div>
+            <div className="col-2">
+              <h3> Mon</h3>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png"
+                alt=""
+              />
+              <div className="weather-forecast-temperature">
+                <strong>22°</strong> 17°
+              </div>
+            </div>
+            <div className="col-2">
+              <h3>Tue</h3>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/48/rain_s_cloudy.png"
+                alt=""
+              />
+              <div className="weather-forecast-temperature">
+                <strong>22°</strong> 17°
+              </div>
+            </div>
+          </div>
+        </div>
+        <span className="githubicon">
+          {" "}
+          <a
+            className="github-icon"
+            href="https://github.com/lmnlfrz/weather-app-github"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <i className="fab fa-github"></i>{" "}
+          </a>{" "}
+        </span>
+        <p className="footer">Coded by Asli Guresci Onder</p>
+      </div>
+    </div>
+  );
 }
