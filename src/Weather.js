0142cea0d6892e25dcc3b38afb06ba54353225fb
01ago <codingmoding01@gmail.com>
2021-04-08 12:13:40 -0400

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -17,7 +17,7 @@ export default function Weather(props) {
       country: response.data.sys.country,
       date: new Date(response.data.dt*1000),
       feelsLike: 3,
-      iconUrl:`https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+      icon:response.data.weather[0].icon,
       humidity: response.data.main.humidity,
       wind: Math.round(response.data.wind.speed) });
     setReady(true);
