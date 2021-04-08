ba375b3409745edd79e0eff5c1188d62f492767d
01ago <codingmoding01@gmail.com>
2021-04-08 12:13:57 -0400

    Create WeatherIcon.js

---
+++ src/WeatherIcon.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactAnimatedWeather from 'react-animated-weather';
+
+export default function WeatherIcon(props){
+    const codeMapping={
+        "01d":"CLEAR_DAY",
+        "01n":"CLEAR_NIGHT",
+        "02d":"PARTLY_CLOUDY",
+        "02n":"PARTLY_CLOUDY",
+        "03d":"CLOUDY",
+        "03n":"CLOUDY",
+        "04d":"CLOUDY",
+        "04n":"CLOUDY",
+        "09d":"RAIN",
+        "09n":"RAIN",
+        "10d":"RAIN",
+        "10n":"RAIN",
+        "11d":"WIND",
+        "11n":"TWIND",
+        "13d":"SNOW",
+        "13n":"SNOW",
+        "50d":"FOG",
+        "50n":"FOG"
+    };
+    
+    return (
+    <ReactAnimatedWeather
+        icon={codeMapping[props.code]}
+        color= "rgb(135, 135, 135)"
+        size={50}
+        animate={true}
+      />);}
+    
