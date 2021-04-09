b5608b5e7b5c0a8acfdfd488c8f7d598c12bba5e
01ago <codingmoding01@gmail.com>
2021-04-09 11:53:47 -0400

    Create TempUnit.js
    convert fahrenheit to celcius vs

---
+++ src/TempUnit.js
@@ -0,0 +1,43 @@
+import React,{useState} from "react";
+
+export default function TempUnit(props){
+    const [unit,setUnit] = useState("celcius");
+   function convertFah(event){
+       event.preventDefault();
+       setUnit("Fahrenheit");
+   }
+   function convertCel(event){
+       event.preventDefault();
+       setUnit("celcius");
+   }
+   
+    if(unit==="celcius"){
+    return(
+        <div className="TempUnit">
+        <span className="temperature"> {Math.round(props.celcius)}</span>
+        <span className="units">
+        <a href="/" className="celcius"> °C | </a>{""}
+          
+          <a href="/" onClick={convertFah} className="fahrenheit active">  °F
+          </a>
+         
+        </span>
+        </div>
+    );}
+    else{
+        let fahTemp= Math.round((props.celcius*9/5)+32);
+        return ( <div className="TempUnit">
+        <span className="temperature"> {fahTemp}</span>
+        <span className="units">
+          <a href="/" className="celcius active">
+            °C |
+          </a>{" "}
+          
+         {  <a href="/" onClick={convertCel} className="fahrenheit">
+            °F
+          </a> }
+        </span>
+        </div>
+    );
+    }
+}
