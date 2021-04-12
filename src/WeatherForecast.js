54bfac4993af9e684d47e103e26ae66a34330513
01ago <codingmoding01@gmail.com>
2021-04-12 12:57:33 -0400

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,46 +1,42 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React,{useState} from "react";
+
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props){
+  let [loaded,setLoaded]= useState(false);
+  let [forecast,setForecast]= useState(null);
+
     function handleResponse(response){
          console.log(response.data); 
+         setForecast(response.data.daily);
+         setLoaded(true);
     }
    console.log(props);
-    let apiKey="5e495a0f30ae782754537f5c56c584c1";
-    let longitude=props.coordinates.lon;
-    let latitude=props.coordinates.lat; 
+    
+    if(loaded){
+      return(
+        <span className="WeatherForecast">  
+        <div className="nextdays">Next 3 days</div>
+ 
+ <span className="nextday1"> <WeatherForecastDay data={forecast[0]}/> </span> 
+ <span className="nextday2">  <WeatherForecastDay data={forecast[1]}/> </span> 
+  <span className="nextday3">  <WeatherForecastDay data={forecast[2]}/></span>  
+ 
+  </span> );
   
-    let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`; 
+    } else{
+
+      let apiKey="5e495a0f30ae782754537f5c56c584c1";
+      let longitude=props.coordinates.lon;
+      let latitude=props.coordinates.lat; 
+      
+      let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`; 
+      
+      
+      axios.get(apiUrl).then(handleResponse);
+      return null;
     
-    
-    axios.get(apiUrl).then(handleResponse);
-    return(
-      <div className="WeatherForecast">  
-      <div className="nextdays">Next 3 days</div>
-<div className="row weather-forecast">
-  <div className="col-2">
-    <h3 className="forecastDays"> Sun</h3>
-    <span className="forecasticon"><WeatherIcon code="01d" size={40} /></span>
-    <div className="weather-forecast-temperature">
-      <strong>22°</strong> 17°
-    </div>
-  </div>
-  <div className="col-2">
-    <h3 className="forecastDays"> Mon</h3>
-    <span className="forecasticon"><WeatherIcon code="50d" size={40} /></span>
-    <div className="weather-forecast-temperature">
-      <strong>22°</strong> 17°
-    </div>
-  </div>
-  <div className="col-2">
-    <h3 className="forecastDays">Tue</h3>
-    <span className="forecasticon"><WeatherIcon code="09d" size={40}/></span>
-    <div className="weather-forecast-temperature">
-      <strong>22°</strong> 17°
-    </div>
-  </div>
-</div>
-</div> );
+}
 }
