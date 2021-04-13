e7e4b707e41989d1e1693277997f49a54e4e0695
01ago <codingmoding01@gmail.com>
2021-04-13 10:24:57 -0400

    Update WeatherForecastDay.js
    useEffect added for forecast update issue

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -4,15 +4,15 @@ import WeatherIcon from "./WeatherIcon";
 export default function WeatherForecastDay(props){
     function maxTemperature(){
         let maxTemp= Math.round(props.data.temp.max);
-        return`${maxTemp}°`
+        return`${maxTemp}°`;
     }
     function minTemperature(){
         let minTemp= Math.round(props.data.temp.min);
-        return`${minTemp}°`
+        return`${minTemp}°`;
     }
 
     function day(){
-let date=new Date (props.data.dt*1000);
+let date=new Date(props.data.dt*1000);
 let day= date.getDay();
 
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -26,25 +26,11 @@ return(
     <span className="col-2"> 
    <span> <h3 className="forecastDays"> {day()}</h3></span>
     <span className="forecasticon"><WeatherIcon code={props.data.weather[0].icon} size={40} /></span>
-   {/*  <div className="weather-forecast-temperature"> */}
+
       <strong className="forecastTemp">{maxTemperature()} {minTemperature()}</strong>
-    {/*   </div> */}
+  
       </span>
   </span>
-  /*  <div className="col-2">
-    <h3 className="forecastDays"> {day()}</h3>
-    <span className="forecasticon"><WeatherIcon code={props.data.weather[0].icon} size={40} /></span>
-    <div className="weather-forecast-temperature">
-      <strong>{maxTemperature()}</strong> {minTemperature()}
-    </div>
-  </div>
-  <div className="col-2">
-    <h3 className="forecastDays">{day()}</h3>
-    <span className="forecasticon"><WeatherIcon code={props.data.weather[0].icon} size={40}/></span>
-    <div className="weather-forecast-temperature">
-      <strong>{maxTemperature()}</strong> {minTemperature()}
-    </div>
-  </div>  */
 
 );
 }
